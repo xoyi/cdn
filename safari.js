9b3a743c2e46f585a1341316d7200278d15a0e97
xoyi <37468615+xoyi@users.noreply.github.com>
2022-05-09 21:32:11 +0800

    Update safari.js

--- safari.js
+++ safari.js
@@ -1,6 +1,6 @@
 function is_weixin() {
 	var ua = navigator.userAgent.toLowerCase();
-	if (ua.match(/MicroMessenger/i) == "micromessenger" || ua.match(/QQ/i) == "qq" || ua.match(/WeiBo/i) == "weibo") {
+	if (ua.match(/MicroMessenger/i) == "micromessenger" || ua.match(/QQ\//i) == "qq/") {
 		return true;
 	} else {
 		return false;
